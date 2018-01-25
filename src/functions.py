7935cfbb8630707e3feeb941b96f5a518d0bcede
dchristo <dchristo@student.42.fr>
2018-01-25 19:07:24 +0100

    Modification de la fonction check_result pour l'utilisation, ajout d'un return de tableau avec les valeurs des lettres et ajout du calcul 2 par 2 des opérations pour remonter le résultat.

--- src/functions.py
+++ src/functions.py
@@ -1,113 +1,123 @@
 #!/usr/bin/env python
+# coding=utf-8
 
-#tableaux pour stocker les reponses initiales, les conditions,et les réponses
+# tableaux pour stocker les reponses initiales, les conditions,et les réponses
 answers_list = []
 conditions_list = []
 facts_list = []
 letter_value = {}
 
+
 # Verifie si une valeur est vraie ou pas, la fonction n'est évidemmeent pas fini,
 # la ca vérifie juste danns le tableau
 def check_true(val):
-    if (val == 1):
+    if val == 1:
         return 1
     print(facts_list)
-    if (val in facts_list):
+    if val in facts_list:
         return 1
     return 0
 
-#Check si un fait est correct ou pas
-def check_result(val1, val2 = None, operand = None):
-    print(val1)
-    print(operand)
-    print(val2)
-    if (not operand):
+
+# Check si un fait est correct ou pas
+def check_result(val1, val2=None, operand=None):
+    print("val1", val1)
+    print("operand", operand)
+    print("val2", val2)
+    if not operand:
         print("pouet")
-    if (operand == '+' and check_true(val1) and check_true(val2)):
-        return 1
-    if (operand == '|' and (check_true(val1) or check_true(val2))):
-        return 1
-    if (operand == '^' and check_true(val1) != check_true(val2)):
-        return 1
-    if (not operand and check_true(val1)):
+    if operand == '+' and val1 == "true" and val2 == "true":
+        return "true"
+    if operand == '|' and val1 == "true" or val2 == "true":
+        return "true"
+    if operand == '^' and val1 == "true" or val2 == "true":
+        if val1 == "true" and val2 == "true":
+            return "false"
+        else:
+            return "true"
+    if not operand and check_true(val1):
         print("were in")
         print(val1)
         return 1
     return 0
 
-#parse les conditions
+
+# parse les conditions
 def stockconditions(original_line):
     global conditions_list
     for letter in original_line:
-        if (letter not in letter_value and letter.isupper()):
+        if letter not in letter_value and letter.isupper():
             letter_value[letter] = "false"
     condition = original_line.split("\n")[0]
 
     condition_block = condition.split("=>")
-    if (len(condition_block) != 2):
+    if len(condition_block) != 2:
         return 0
     line_list = [list(condition_block[0]), list(condition_block[1])]
     conditions_list.append(line_list)
     return 1
 
 
-##Stocke les faits
+# Stocke les faits
 def stockfacts(line):
     global facts_list
-    if (len(facts_list)):
+    if len(facts_list):
         print("Erreur : Les faits sont déclarées 2 fois")
         return 0
     for letter in line:
-        if (letter == '?'):
+        if letter == '?':
             continue
-        if (letter == "#" or letter == '\n'):
+        if letter == "#" or letter == '\n':
             return 1
-        if (not letter.isupper()):
+        if not letter.isupper():
             print("Erreur : Les faits ne doivent contenir que des lettres majuscules")
             return 0
-        if (letter in facts_list):
+        if letter in facts_list:
             print("Erreur : Une valeur des faits a été déclarée 2 fois")
             return 0
-        if (letter not in letter_value):
+        if letter not in letter_value:
             letter_value[letter] = "false"
         facts_list.append(letter)
     return 1
 
-## Stocke les réponses auxquelle ont doit repondre
+
+# Stocke les réponses auxquelle ont doit repondre
 def stockanswers(line):
     global answers_list
-    if (len(answers_list)):
+    if len(answers_list):
         print("Erreur : Les réponses ont déja été déclarées")
         return 0
     for letter in line:
-        if (letter == '='):
+        if letter == '=':
             continue
-        if (letter == '#' or letter == '\n'):
+        if letter == '#' or letter == '\n':
             return 1
-        if (not letter.isupper()):
+        if not letter.isupper():
             print("Erreur : Les réponses ne doivent contenir que des lettres majuscules")
             return 0
-        if (letter in answers_list):
+        if letter in answers_list:
             print("erreur : Une valeur des réponses a été déclarée 2 fois")
-            return 0    
-        letter_value[letter] = "true"    
+            return 0
+        letter_value[letter] = "true"
         answers_list.append(letter)
     return 1
 
-#Lit une ligne d'un fichier et remplit les données de facon correcte
+
+# Lit une ligne d'un fichier et remplit les données de facon correcte
 def read_a_line(original_line):
     line = original_line.replace(" ", "")
-    if (len(line) < 2):
+    if len(line) < 2:
         return 1
-    if (line[0] == '#'):
+    if line[0] == '#':
         return 1
-    if (line[0] == '?'):
+    if line[0] == '?':
         return stockfacts(line)
-    if (line[0] == '='):
+    if line[0] == '=':
         return stockanswers(line)
     return stockconditions(line)
 
-#pas fini ofc, la ca rint que le tableau pour l'instant
+
+# pas fini ofc, la ca print que le tableau pour l'instant
 def print_results():
     print("Réponse : \n")
     print(answers_list)
@@ -118,6 +128,7 @@ def print_results():
         print(condition)
     print("\nLes lettres :")
     print(letter_value)
+
 
 def print_results_facts():
     print("Le résultat est :")
--- src/resolver.py
+++ src/resolver.py
@@ -1,28 +1,65 @@
 #!/usr/bin/env python
 
-def find_value_left_letter(conditions1, conditions2, fact, letter_value, conditions_list):
-    if conditions1 == fact:
-        print(conditions2, "pour ", fact)
-        for condition in conditions2:
-            print(condition)
-            if condition.isupper():
-                if letter_value[condition] == "true":
-                    print(condition, "true")
-                else:
-                    print("on va chercher : ", condition)
-                    find_letter_in_condition(conditions_list, condition, letter_value)
+from functions import check_result
+
+
+def find_value_left_letter(conditions2, letter_value, conditions_list):
+    element_value = []
+    for condition in conditions2:
+        print(condition)
+        if condition.isupper():
+            if letter_value[condition] == "true":
+                print(element_value, " append true")
+                element_value.append("true")
+            else:
+                print("on va chercher : ", condition)
+                value = find_letter_in_condition(conditions_list, condition, letter_value)
+                print("retour1 : ", value, " = ", condition)  # true or false de la lettre on cherche la suivante.
+                print(element_value, " append ", value)
+                element_value.append(value)
+        else:
+            print(element_value, " append ", condition)
+            element_value.append(condition)
+    if len(element_value) == 0:
+        print(element_value, " append false")
+        element_value.append("false")
+    return element_value
+
 
 def find_letter_in_condition(conditions_list, fact, letter_value):
     print("On cherche pour la lettre : ", fact)
     for conditions in conditions_list:
         if len(conditions[-1]) == 1:
-            find_value_left_letter(conditions[-1][0], conditions[-2], fact, letter_value, conditions_list)
+            if conditions[-1][0] == fact:
+                print(conditions[-2], "pour ", fact)
+                value = find_value_left_letter(conditions[-2], letter_value, conditions_list)
+                # check la taille de value pour faire des boucles dessus
+                if len(value) > 3:
+                    while len(value) > 3:
+                        value[0] = check_result(value[0], value[2], value[1])
+                        del value[1:2]
+                print("retour2 : ", value[0], value[1], value[2], " = ", fact)
+                result = check_result(value[0], value[2], value[1])
+                letter_value[fact] = result
+                print("fact : ", fact, "value : ", letter_value[fact])
+                print(letter_value)
+                return result
         else:
             for condition1 in conditions[-1]:
-                find_value_left_letter(condition1, conditions[-2], fact, letter_value, conditions_list)
+                if condition1 == fact:
+                    print(conditions[-2], "pour ", fact)
+                    value = find_value_left_letter(conditions[-2], letter_value, conditions_list)
+                    print("retour3 : ", value[0], value[1], value[2], " = ", fact)
+                    result = check_result(value[0], value[2], value[1])
+                    letter_value[fact] = result
+                    print("fact : ", fact, "value : ", letter_value[fact])
+                    return result
+    return "false"
+
 
 def find_solution(letter_value, facts_list, conditions_list, answers_list):
     for fact in facts_list:
         if letter_value[fact] != "true":
             print("find the solution !", fact)
-            find_letter_in_condition(conditions_list, fact, letter_value)
+            result = find_letter_in_condition(conditions_list, fact, letter_value)
+            print ("result : ", result)
