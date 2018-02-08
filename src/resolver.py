31a20ace54cfd562a3744405e96c4540448dc564
dchristo <dchristo@student.42.fr>
2018-02-08 17:03:16 +0100

    On a trouver le meilleur index (par le nombre de lettre a chercher)

--- src/resolver.py
+++ src/resolver.py
@@ -4,9 +4,18 @@
 from functions import check_result
 
 
+def len_array(array):
+    length = 0
+    for index in array:
+        if index != '!':
+            length += 1
+    return length
+
+
 def find_value_letter(conditions2, letter_value, conditions_list):
     element_value = []
-    for index, condition in conditions2:
+    index = 0
+    for condition in conditions2:
         print(condition)
         if condition.isupper():
             if letter_value[condition] == "true":
@@ -29,6 +38,7 @@ def find_value_letter(conditions2, letter_value, conditions_list):
         else:
             print(element_value, " append ", condition)
             element_value.append(condition)
+        index += 1
     if len(element_value) == 0:
         print(element_value, " append false")
         element_value.append("false")
@@ -37,6 +47,27 @@ def find_value_letter(conditions2, letter_value, conditions_list):
 
 def find_letter_in_condition(conditions_list, fact, letter_value):
     print("On cherche pour la lettre : ", fact)
+    save_len = 0
+    save_index = 0
+    index = 0
+    for conditions in conditions_list:
+        if len(conditions[-1]) == 1:
+            if conditions[-1][0] == fact:
+                print(len_array(conditions[0]), save_len, index, save_index)
+                if len_array(conditions[0]) < save_len or save_len == 0:
+                    print("in1")
+                    save_index = index
+                    save_len = len(conditions[0])
+        else:
+            for condition1 in conditions[-1]:
+                if condition1 == fact:
+                    print(len_array(conditions[0]), len_array(conditions[-1]), save_len, index, save_index)
+                    if (len_array(conditions[0]) + len_array(conditions[-1]) - 1) < save_len or save_len == 0:
+                        print("in")
+                        save_index = index
+                        save_len = len_array(conditions[0]) + len_array(conditions[-1] - 1)
+        index += 1
+    print("la taille est de : ", save_len, "et l'index est : ", save_index, conditions_list[save_index])
     for conditions in conditions_list:
         if len(conditions[-1]) == 1:
             if conditions[-1][0] == fact:
@@ -67,14 +98,14 @@ def find_letter_in_condition(conditions_list, fact, letter_value):
                 for index, condition1 in conditions[-1]:
                     if condition1 != fact:
                         value = find_value_letter(condition1, letter_value, conditions_list)
-                        # Si on a A + B + C on va faire A + B et supprimer + B remplacer A par le résultat (true or false)
+                        # Si on a A + B + C on va faire A + B et supprimer + B remplacer A par le résultat (true or false) on aura true/false + C
                         while len(value) > 3:
                             value[0] = check_result(value[0], value[2], value[1])
                             del value[1:2]
                         # Et ici nous allons fait true/false + C
                         letter_value[condition1] = check_result(value[0], value[2], value[1])
                         conditions[-1][index] = letter_value[condition1]
-                    # faire le calcul global pour finir
+                        # faire le calcul global pour finir
     return "false"
 
 
