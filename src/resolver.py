5af5431e26d6706a5db1128e89443fb4b4f17377
dchristo <dchristo@student.42.fr>
2018-02-08 19:20:09 +0100

    Replacing de code avec le nouveaux (sans la boucle) quelque bug a tester encore

--- src/resolver.py
+++ src/resolver.py
@@ -65,48 +65,87 @@ def find_letter_in_condition(conditions_list, fact, letter_value):
                     if (len_array(conditions[0]) + len_array(conditions[-1]) - 1) < save_len or save_len == 0:
                         print("in")
                         save_index = index
-                        save_len = len_array(conditions[0]) + len_array(conditions[-1] - 1)
+                        save_len = (len_array(conditions[0]) + len_array(conditions[-1]) - 1)
         index += 1
     print("la taille est de : ", save_len, "et l'index est : ", save_index, conditions_list[save_index])
-    for conditions in conditions_list:
-        if len(conditions[-1]) == 1:
-            if conditions[-1][0] == fact:
-                print(conditions[-2], "pour ", fact)
-                value = find_value_letter(conditions[-2], letter_value, conditions_list)
-                while len(value) > 3:
-                    value[0] = check_result(value[0], value[2], value[1])
-                    del value[1:2]
-                print("retour2 : ", value[0], value[1], value[2], " = ", fact)
+    # for conditions in conditions_list:
+    #     if len(conditions[-1]) == 1:
+    #         if conditions[-1][0] == fact:
+    #             print(conditions[-2], "pour ", fact)
+    #             value = find_value_letter(conditions[-2], letter_value, conditions_list)
+    #             while len(value) > 3:
+    #                 value[0] = check_result(value[0], value[2], value[1])
+    #                 del value[1:2]
+    #             print("retour2 : ", value[0], value[1], value[2], " = ", fact)
+    #             letter_value[fact] = check_result(value[0], value[2], value[1])
+    #             print("fact : ", fact, "value : ", letter_value[fact])
+    #             print(letter_value)
+    #             return letter_value[fact]
+    #     else:
+    #         find = "false"
+    #         for condition1 in conditions[-1]:
+    #             if condition1 == fact:
+    #                 find = "true"
+    #                 # Si on trouve notre bonheur ici, il faudra aussi calculer les autres pour savoir le résultat éxact
+    #                 print(conditions[-2], "pour ", fact)
+    #                 value = find_value_letter(conditions[-2], letter_value, conditions_list)
+    #                 print("retour3 : ", value[0], value[1], value[2], " = ", fact)
+    #                 letter_value[fact] = check_result(value[0], value[2], value[1])
+    #                 print("fact : ", fact, "value : ", letter_value[fact])
+    #                 print(letter_value)
+    #                 return letter_value[fact]
+    #         if find == "true":
+    #             for index, condition1 in conditions[-1]:
+    #                 if condition1 != fact:
+    #                     value = find_value_letter(condition1, letter_value, conditions_list)
+    #                     # Si on a A + B + C on va faire A + B et supprimer + B remplacer A par le résultat (true or false) on aura true/false + C
+    #                     while len(value) > 3:
+    #                         value[0] = check_result(value[0], value[2], value[1])
+    #                         del value[1:2]
+    #                     # Et ici nous allons fait true/false + C
+    #                     letter_value[condition1] = check_result(value[0], value[2], value[1])
+    #                     conditions[-1][index] = letter_value[condition1]
+    #                     # faire le calcul global pour finir
+    # return "false"
+    if len(conditions_list[save_index][-1]) == 1:
+        print(conditions_list[save_index][-2], "pour ", fact)
+        value = find_value_letter(conditions_list[save_index][-2], letter_value, conditions_list)
+        while len(value) > 3:
+            value[0] = check_result(value[0], value[2], value[1])
+            del value[1:2]
+        print("retour2 : ", value[0], value[1], value[2], " = ", fact)
+        letter_value[fact] = check_result(value[0], value[2], value[1])
+        print("fact : ", fact, "value : ", letter_value[fact])
+        print(letter_value)
+        return letter_value[fact]
+    else:
+        find = "false"
+        for condition1 in conditions_list[save_index][-1]:
+            if condition1 == fact:
+                find = "true"
+                # Si on trouve notre bonheur ici, il faudra aussi calculer les autres pour savoir le résultat éxact
+                print(conditions_list[save_index][-2], "pour ", fact)
+                value = find_value_letter(conditions_list[save_index][-2], letter_value, conditions_list)
+                print("retour3 : ", value[0], value[1], value[2], " = ", fact)
                 letter_value[fact] = check_result(value[0], value[2], value[1])
                 print("fact : ", fact, "value : ", letter_value[fact])
                 print(letter_value)
                 return letter_value[fact]
-        else:
-            find = "false"
-            for condition1 in conditions[-1]:
-                if condition1 == fact:
-                    find = "true"
-                    # Si on trouve notre bonheur ici, il faudra aussi calculer les autres pour savoir le résultat éxact
-                    print(conditions[-2], "pour ", fact)
-                    value = find_value_letter(conditions[-2], letter_value, conditions_list)
-                    print("retour3 : ", value[0], value[1], value[2], " = ", fact)
-                    letter_value[fact] = check_result(value[0], value[2], value[1])
-                    print("fact : ", fact, "value : ", letter_value[fact])
-                    print(letter_value)
-                    return letter_value[fact]
-            if find == "true":
-                for index, condition1 in conditions[-1]:
-                    if condition1 != fact:
-                        value = find_value_letter(condition1, letter_value, conditions_list)
-                        # Si on a A + B + C on va faire A + B et supprimer + B remplacer A par le résultat (true or false) on aura true/false + C
-                        while len(value) > 3:
-                            value[0] = check_result(value[0], value[2], value[1])
-                            del value[1:2]
-                        # Et ici nous allons fait true/false + C
-                        letter_value[condition1] = check_result(value[0], value[2], value[1])
-                        conditions[-1][index] = letter_value[condition1]
-                        # faire le calcul global pour finir
-    return "false"
+        if find == "true":
+            index = 0
+            for condition1 in conditions_list[save_index][-1]:
+                if condition1 != fact:
+                    value = find_value_letter(condition1, letter_value, conditions_list)
+                    # Si on a A + B + C on va faire A + B et supprimer + B remplacer A par le résultat (true or false) on aura true/false + C
+                    while len(value) > 3:
+                        value[0] = check_result(value[0], value[2], value[1])
+                        del value[1:2]
+                    # Et ici nous allons fait true/false + C
+                    letter_value[condition1] = check_result(value[0], value[2], value[1])
+                    conditions_list[save_index][-1][index] = letter_value[condition1]
+                    # faire le calcul global pour finir
+                index += 1
+        return "false"
 
 
 def find_solution(letter_value, facts_list, conditions_list):
